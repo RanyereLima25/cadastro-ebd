b7f31d2644172a3448c0d41b89939ca71fbc8057
RanyereLima25 <limaranyere@gmail.com>
2025-05-15 20:09:00 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,14 +1,13 @@
-
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 class Pessoa(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100))
-    cpf = db.Column(db.String(20))
+    nome = db.Column(db.String(100), nullable=False)
+    cpf = db.Column(db.String(20), unique=True, nullable=False)
     nascimento = db.Column(db.String(20))
-    email = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
     telefone = db.Column(db.String(20))
     matricula = db.Column(db.String(50))
     curso = db.Column(db.String(100))
@@ -21,3 +20,6 @@ class Pessoa(db.Model):
     bairro = db.Column(db.String(50))
     cidade = db.Column(db.String(50))
     estado = db.Column(db.String(50))
+
+    def __repr__(self):
+        return f'<Pessoa {self.nome}>'
