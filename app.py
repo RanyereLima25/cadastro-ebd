5e126bbaaefd4ab95f7433a14649ee91d1b09abe
RanyereLima25 <limaranyere@gmail.com>
2025-05-16 11:10:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,8 @@ def relatorios():
 def graficos():
     return render_template('graficos.html')
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True, host='0.0.0.0')
