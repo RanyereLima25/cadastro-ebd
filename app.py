c35184ad3e709cd860eaf0a9288ea940c8fcda01
RanyereLima25 <limaranyere@gmail.com>
2025-05-15 20:10:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,22 @@ from flask import Flask, render_template, request, redirect, url_for
 from models import db, Pessoa
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pessoas.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db.init_app(app)
 
-@app.route('/', methods=['GET', 'POST'])
+# Cria o banco
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
 def index():
+    pessoas = Pessoa.query.all()
+    return render_template('index.html', pessoas=pessoas)
+
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro():
     if request.method == 'POST':
         nova_pessoa = Pessoa(
             nome=request.form['nome'],
@@ -29,54 +39,5 @@ def index():
         )
         db.session.add(nova_pessoa)
         db.session.commit()
-        return redirect('/visualizar')
-    return render_template('index.html')
-
-@app.route('/visualizar')
-def visualizar():
-    pessoas = Pessoa.query.all()
-    return render_template('visualizar.html', pessoas=pessoas)
-
-@app.route('/excluir/<int:id>')
-def excluir(id):
-    pessoa = Pessoa.query.get(id)
-    db.session.delete(pessoa)
-    db.session.commit()
-    return redirect('/visualizar')
-
-@app.route('/editar/<int:id>', methods=['GET', 'POST'])
-def editar(id):
-    pessoa = Pessoa.query.get(id)
-    if request.method == 'POST':
-        pessoa.nome = request.form['nome']
-        pessoa.cpf = request.form['cpf']
-        pessoa.nascimento = request.form['nascimento']
-        pessoa.email = request.form['email']
-        pessoa.telefone = request.form['telefone']
-        pessoa.matricula = request.form['matricula']
-        pessoa.curso = request.form['curso']
-        pessoa.sala = request.form['sala']
-        pessoa.ano_ingresso = request.form['ano_ingresso']
-        pessoa.cep = request.form['cep']
-        pessoa.rua = request.form['rua']
-        pessoa.numero = request.form['numero']
-        pessoa.complemento = request.form['complemento']
-        pessoa.bairro = request.form['bairro']
-        pessoa.cidade = request.form['cidade']
-        pessoa.estado = request.form['estado']
-        db.session.commit()
-        return redirect('/visualizar')
-    return render_template('editar.html', pessoa=pessoa)
-
-@app.route('/relatorios')
-def relatorios():
-    return render_template('relatorios.html')
-
-@app.route('/graficos')
-def graficos():
-    return render_template('graficos.html')
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+        return redirect(url_for('index'))
+    return render_template('cadastro.html')
