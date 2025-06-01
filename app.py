407cd65eb7e79c6fd5e94cc7398153762b0d1fd4
RanyereLima25 <limaranyere@gmail.com>
2025-05-31 22:54:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -269,5 +269,5 @@ def excluir(pessoa_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    #app.run(host='0.0.0.0', port=5000, debug=True)
-    streamlit run app.py
+    app.run(host='0.0.0.0', port=5000, debug=True)
+   
