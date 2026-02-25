c02f0a932d5418b90e2066afa3297d45322c7888
RanyereLima25 <limaranyere@gmail.com>
2026-02-24 23:08:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -189,6 +189,8 @@ def visualizar():
 # =============================
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()  # Cria tabelas no Supabase
-    app.run(debug=True)
+        db.create_all()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
 
