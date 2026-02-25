3b4394ec6fc94ef6d7185b1d3f9b89e33291a2b1
RanyereLima25 <limaranyere@gmail.com>
2026-02-25 02:58:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -201,7 +201,8 @@ def visualizar():
 # EXECUÇÃO
 # =============================
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+with app.app_context():
+    db.create_all()
 
+if __name__ == "__main__":
+    app.run()
